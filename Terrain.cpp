b9e8ddd66b28f6c75794b68c7a1821c00e8a74e3
Elray84 <elianraynaud@gmail.com>
2019-01-24 19:17:06 +0100

    Le bon terrain

--- Terrain.cpp
+++ Terrain.cpp
@@ -1,3 +1,15 @@
-#include <iostream>
-#include <cstdlib>
-#include "Terrain.hpp"
+class Terrain {
+  Terrain(bool charger, std::String fichierSauvegarde){
+    /* A compléter :
+    *
+    * Il faut parser le fichierSauvegarde puis créer les différents objets
+    * (joueurs, entités, ...) avec les infos dans le fichierSauvegarde.
+    *
+    * Il faudra définir un pattern pour mettre en forme les infos...
+    *
+    */
+  };
+  static Terrain monTerrain;
+  std::vector<Entite> mesEntites;
+  std::vector<Joueur> mesJoueurs;
+}
